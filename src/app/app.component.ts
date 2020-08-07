a881b9e522a9bef739e22fc98bf16b222f95a522
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2020-08-07 09:19:03 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: '<div class="test"> {{title}}</div>',
+
+  // CSS này chỉ có tác dùng với templateUrl
   styleUrls: ['./app.component.scss']
+  // styles: ['.test{ width: 500px; height: 500px; background-color: greenyellow; color: red; }']
 })
 export class AppComponent {
-  title = 'app1-componentVariable';
+  // biến này đc gọi ở template html
+  title: string = 'app1-componentVariable';
+  numberOfClick: number= 0;
+
+  // function sẽ đc gọi ở template
+  public clickButton(){
+    this.numberOfClick++;
+  }
 }
