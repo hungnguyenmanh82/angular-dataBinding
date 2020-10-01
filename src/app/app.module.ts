8700b89cdd2eb4be70d03c47719f6004cc33b64d
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2020-10-01 14:10:13 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+//
 import { AppComponent } from './app.component';
 import { BindingComponent } from './binding/binding.component';
 import { Binding2Component } from './binding2/binding2.component';
 import { NgForComponent } from './ng-for/ng-for.component';
 import { HtmlAttributeComponent } from './html-attribute/html-attribute.component';
-import { FormsModule } from '@angular/forms';
 import { NgClassComponent } from './ng-class/ng-class.component';
 import { NgStyleComponent } from './ng-style/ng-style.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 import { Binding2RadioComponent } from './binding2-radio/binding2-radio.component';
 import { InputKeyupComponent } from './input-keyup/input-keyup.component';
-
 
 @NgModule({
   declarations: [
@@ -29,13 +28,9 @@ import { InputKeyupComponent } from './input-keyup/input-keyup.component';
     NgSwitchComponent,
     Binding2RadioComponent,
     InputKeyupComponent,
-
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
