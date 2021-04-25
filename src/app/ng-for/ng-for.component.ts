4fc7178072e3ef6552c6a0d75c85b43368887e3c
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2021-04-25 14:52:10 +0700

    update

--- src/app/ng-for/ng-for.component.ts
+++ src/app/ng-for/ng-for.component.ts
@@ -4,37 +4,46 @@ import { Customer } from './Customer';
 @Component({
   selector: 'app-ng-for',
   templateUrl: './ng-for.component.html',
-  styleUrls: ['./ng-for.component.scss']
+  styleUrls: ['./ng-for.component.scss'],
 })
 export class NgForComponent implements OnInit {
+  items: Customer[] = [
+    new Customer(1, 'hung', 14),
+    new Customer(2, 'khien', 14),
+    new Customer(3, 'kien', 14),
+  ];
 
-  items:Customer[] = [new Customer(1,"hung",14),new Customer(2,"khien",14),new Customer(3,"kien",14) ];
-  itemChangedIds:Customer[] = [new Customer(1,"hungChanged",14),new Customer(2,"khien Ko changeId",14),new Customer(5,"kienChanged",14) ];
-  itemChangedNames:Customer[] = [new Customer(1,"hungChanged",14),new Customer(2,"khienChanged",14),new Customer(3,"kien",14) ];
+  newItems: Customer[] = [
+    new Customer(1, 'hungChanged', 15),
+    new Customer(2, 'khien Ko changeId', 16),
+    new Customer(5, 'kienChanged', 14),
+  ];
+
   customers: Customer[];
 
-  custome1: Customer = new Customer(1,'chân chính',8);
-  constructor() { }
+  custome1: Customer = new Customer(1, 'chân chính', 8);
+  constructor() {}
 
   ngOnInit(): void {
     this.customers = this.items;
   }
 
-  clickButton(name:string){
+  clickButton(name: string) {
     window.alert(name);
   }
 
-  resetList(){
+  resetList() {
     this.customers = this.items;
   }
 
-  changeIds(){
-    this.customers = this.itemChangedIds;
+  changeNewArray() {
+    this.customers = this.newItems;
   }
 
-  changeIds2(){
+  changeItemOfArray() {
     // ngay cả change thành phần của biến vẫn đc update vào html (lạ thật)
-    this.customers[0].name = 'hungchanged';
+    this.customers[0].name = 'aaaaa';
+    this.customers[2].name = 'bbbbb';
   }
 
   /**
@@ -42,16 +51,16 @@ export class NgForComponent implements OnInit {
    * @param index : là vị trí trong array của *ngFor
    * @param customer: là object của array tại vị trí index
    */
-  trackById(index:number, customer: Customer): number{
+  trackById(index: number, customer: Customer): number {
     return customer.id;
   }
 
-    /**
+  /**
    *
    * @param index : là vị trí trong array của *ngFor
    * @param customer: là object của array tại vị trí index
    */
-  trackByName(index:number, customer: Customer): string{
+  trackByName(index: number, customer: Customer): string {
     return customer.name;
   }
 }
